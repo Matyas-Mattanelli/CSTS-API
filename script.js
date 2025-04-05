e8c621323d02dacbf519102a9b143f50b07f9d37
Matyas-Mattanelli <91661261+Matyas-Mattanelli@users.noreply.github.com>
2025-04-05 20:54:40 +0200

    Implementing filtering

--- script.js
+++ script.js
@@ -1,12 +1,73 @@
 // Get the div for showing the results
 const divResults = document.getElementById("results");
 const divMsg = document.getElementById("msg");
+const filters = document.getElementById("filters");
 
 // Get the input element
 const input = document.getElementById("input");
 
 // Specify the headers for the table with results
 const columns = ["IDT", "Jméno", "Klub", "Stát", "Partner/ka", "ID soutěže", "ID eventu", "Typ", "Věková skupina", "Třída", "Disciplína", "Kategorie", "Datum", "Jméno eventu", "Pozice", "Počet účastníků", "Body", "Finále", "Celkem body", "Celkem finále"];
+const uncheckedColumns = ["IDT", "Stát", "ID soutěže", "ID eventu", "Kategorie", "Jméno eventu"];
+
+// Define a function bulding the filters
+function buildFilters() {
+    // Loop through the columns and create checkbox and label for each
+    columns.forEach(col => {
+        // Create checkbox
+        const colClean = col.toLowerCase().replace(" ", "_").replace("/", "");
+        const checkbox = document.createElement("input");
+        checkbox.type = 'checkbox';
+        checkbox.name = colClean;
+        checkbox.value = colClean;
+        checkbox.id = colClean;
+        if (uncheckedColumns.indexOf(col) === -1) {
+            checkbox.checked = true;
+        } else {
+            checkbox.checked = false;
+        }
+        checkbox.className = "filters";
+        checkbox.onchange = () => {filterTable(col)};
+        filters.appendChild(checkbox);
+
+        // Create label
+        const label = document.createElement('label');
+        label.for = colClean;
+        label.textContent = col;
+        label.className = "filters-label";
+        filters.appendChild(label);
+    });
+}
+
+// Define a function filtering the table based on a given filter
+function filterTable(col) {
+        // Find the filter
+        const filter = document.getElementById(col.toLowerCase().replace(" ", "_").replace("/", ""));
+        
+        // Find the corresponding cells
+        const cells = document.querySelectorAll(`table tr :is(td, th):nth-child(${columns.indexOf(col)+1})`);
+        
+        // Hide or show the cells
+        cells.forEach(cell => {
+            console.log(cell.style.display);
+            cell.style.display = filter.checked === true ? 'table-cell' : 'none'
+        });
+}
+
+// Define a function showing or hiding the filters
+const filterBtn = document.getElementById('show-filters');
+function showFilters() {
+    // Hide if shown
+    if (filters.style.display === 'block') {
+        filters.style.display = 'none';
+        filterBtn.textContent = 'Show filters'
+
+    // Show if hidden
+    } else {
+        filters.style.display = 'block';
+        filterBtn.textContent = 'Hide filters'
+    }
+}
 
 // Define a function building a table from the given data
 function buildTable(data) {
@@ -56,6 +117,9 @@ function buildTable(data) {
 
         // Show the table
         divResults.appendChild(table);
+
+        // Filter the columns
+        columns.forEach(col => {filterTable(col)});
     }
 }
 
@@ -91,7 +155,13 @@ function fetchData(input, callback) {
                 return response.json();
             }
         })
-        .then(data => callback(data))
+        .then(data => {
+            if (data.length === 0) {
+                printNoResults(input);    
+            } else {
+                callback(data)
+            }
+        })
         .catch(error => {
             printNoResults(input);
             console.error(`Failed to fetch: `, error);
@@ -102,3 +172,6 @@ function fetchData(input, callback) {
 function printNoResults(input) {
     divMsg.textContent = `Výsledky pro ${input} nebyly nalezeny`;
 }
+
+// Build the filters
+buildFilters();
